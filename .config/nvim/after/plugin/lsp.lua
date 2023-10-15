574a9a57cc390756460875f5772265d93b8aba7a
wilo01 <wilk9496@gmail.com>
2023-10-15 10:09:03 +0200

    Added formatter on save and ignore case sensitive search

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -22,6 +22,7 @@ lsp.set_preferences({
 
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
+    lsp_zero.buffer_autoformat()
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
