b9e27966f2222182901f6542a6e3ed0b6ee5a81e
wilo01 <wilk9496@gmail.com>
2024-01-29 17:18:46 +0100

    Added netrw new features

--- .config/nvim/lua/DarO/init.lua
+++ .config/nvim/lua/DarO/init.lua
@@ -32,3 +32,7 @@ vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.opt.undodir = "~/.vim/undodir"
+vim.g.netrw_special_syntax = 3
+vim.g.netrw_liststyle = 3
+vim.g.netrw_altv = 1
+vim.g.netrw_keepdir=0
