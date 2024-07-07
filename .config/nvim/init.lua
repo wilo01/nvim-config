55eb241bd83d59a3bf2a231e8266eff0bd16135b
Dariusz Wilk <dariuszw@timedatasecurity.com>
2024-07-07 19:53:34 +0100

    JIRA: #Linux
    Logs:
    - Added startup time on boot

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
 require("DarO")
-print("Hello DarO")
+_G.start_time = vim.loop.hrtime()
+vim.api.nvim_command('autocmd VimEnter * lua print_startup_time()')
+
+function print_startup_time()
+    local elapsed_time = (vim.loop.hrtime() - _G.start_time) / 1e6
+    print(string.format("Hello DarO, Neovim startup time: %.2f ms", elapsed_time))
+end
