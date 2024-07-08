e4ade83876b1e966dfbfe939121e33ba896ec92f
Dariusz Wilk <dariuszw@timedatasecurity.com>
2024-07-08 13:13:35 +0100

    JIRA: #Linux
    Logs:
    - Added new feature on save
    - Adding edited files to harpoon on save

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -7,3 +7,16 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_prev() end)
 vim.keymap.set("n", "<C-l>", function() ui.nav_next() end)
+
+function _G.add_file_and_notify()
+  local file_name = vim.fn.expand('%:t')
+  mark.add_file()
+  vim.notify("File: " .. file_name .. " added to Harpoon", vim.log.levels.INFO)
+end
+
+vim.cmd [[
+  augroup HarpoonAutoAdd
+    autocmd!
+    autocmd BufWritePost * lua add_file_and_notify()
+  augroup END
+]]
