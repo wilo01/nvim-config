d9e0a8d11bfd8bb3143174e8d062f3ee132cd07c
Dariusz Wilk <dariuszw@timedatasecurity.com>
2024-07-08 14:18:13 +0100

    JIRA: #Linux
    Logs:
    - Updated code for startup printing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
 require("DarO")
-_G.start_time = vim.loop.hrtime()
+vim.g.startup_time = vim.loop.hrtime()
 vim.api.nvim_command('autocmd VimEnter * lua print_startup_time()')
 
 function print_startup_time()
-    local elapsed_time = (vim.loop.hrtime() - _G.start_time) / 1e6
+    local elapsed_time = (vim.loop.hrtime() - vim.g.startup_time) / 1e6
     print(string.format("Hello DarO, Neovim startup time: %.2f ms", elapsed_time))
 end
