7c98245a14c749341981b76553fe93cd4adb4b0f
Dariusz Wilk <dariuszw@timedatasecurity.com>
2024-07-14 21:47:59 +0100

    JIRA: #Linux
    Logs:
    - Updated harpoon scripts to match with harpoon2

--- .config/nvim-prime/lua/DarO/lazy/harpoon.lua
+++ .config/nvim-prime/lua/DarO/lazy/harpoon.lua
@@ -1,19 +1,26 @@
 return {
    "ThePrimeagen/harpoon",
    branch = "harpoon2",
+   dependencies = { "nvim-lua/plenary.nvim" },
    config = function()
-      local mark = require("harpoon.mark")
-      local ui = require("harpoon.ui")
+      local harpoon = require("harpoon")
+      local list = harpoon:list()
+      harpoon:setup()
 
-      vim.keymap.set("n", "<leader>a", mark.add_file)
-      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(list) end)
+      vim.keymap.set("n", "<leader>a", function() list:add() end)
 
-      vim.keymap.set("n", "<C-h>", function() ui.nav_prev() end)
-      vim.keymap.set("n", "<C-l>", function() ui.nav_next() end)
+      vim.keymap.set("n", "<C-1>", function() list:select(1) end)
+      vim.keymap.set("n", "<C-2>", function() list:select(2) end)
+      vim.keymap.set("n", "<C-3>", function() list:select(3) end)
+      vim.keymap.set("n", "<C-4>", function() list:select(4) end)
+
+      vim.keymap.set("n", "<C-h>", function() list:prev() end)
+      vim.keymap.set("n", "<C-l>", function() list:next() end)
 
       function _G.add_file_and_notify()
          local file_name = vim.fn.expand('%:t')
-         mark.add_file()
+         list:add()
          vim.notify("File: " .. file_name .. " added to Harpoon", vim.log.levels.INFO)
       end
 
