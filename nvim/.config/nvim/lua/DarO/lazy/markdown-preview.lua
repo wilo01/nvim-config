e8a055d9a65062ae251e5b763f00aa1104661240
Wilo01 <wilk9496@gmail.com>
2024-10-03 15:43:36 +0100

    JIRA: #Linux
    Logs:
    - markdown-preview in new window always
    - Fixed config

--- nvim/.config/nvim/lua/DarO/lazy/markdown-preview.lua
+++ nvim/.config/nvim/lua/DarO/lazy/markdown-preview.lua
@@ -1,8 +1,18 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+   "iamcco/markdown-preview.nvim",
+   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+   ft = { "markdown" },
+   build = function() vim.fn["mkdp#util#install"]() end,
+   config = function()
+      vim.cmd([[do FileType]])
+      vim.cmd([[
+         function OpenMarkdownPreview (url)
+            let cmd = "google-chrome-stable --new-window " . shellescape(a:url) . " &"
+            silent call system(cmd)
+         endfunction
+      ]])
+      vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+   end,
 }
 
 -- Old install using yarn
