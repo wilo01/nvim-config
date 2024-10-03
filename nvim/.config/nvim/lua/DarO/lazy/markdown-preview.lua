8b1dca7dd4b8d2e11bcf886e04789afb79e9583f
Wilo01 <wilk9496@gmail.com>
2024-10-03 15:06:08 +0100

    JIRA: #Linux
    Logs:
    - Updated lazy install of markdown-preview

--- nvim/.config/nvim/lua/DarO/lazy/markdown-preview.lua
+++ nvim/.config/nvim/lua/DarO/lazy/markdown-preview.lua
@@ -1,12 +1,25 @@
 return {
-   "iamcco/markdown-preview.nvim",
-   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-   build = "cd app && yarn install",
-   init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-   end,
-   ft = { "markdown" },
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
 }
+
+-- Old install using yarn
+-- return {
+--    "iamcco/markdown-preview.nvim",
+--    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--    build = "cd app && yarn install",
+--    init = function()
+--       vim.g.mkdp_auto_start = 1              -- Automatically start preview when opening markdown file
+--       vim.g.mkdp_auto_close = 1              -- Automatically close preview when switching buffers
+--       vim.g.mkdp_refresh_slow = 0            -- Refresh preview as you type (live preview)
+--       vim.g.mkdp_command_for_global = 1      -- Allow using preview commands globally
+--       vim.g.mkdp_browser = 'google-chrome'   -- Change default browser to Google Chrome
+--       vim.g.mkdp_filetypes = { "markdown" }  -- Restrict to markdown files
+--    end,
+--    ft = { "markdown" },
+-- }
 
 -- Markdown keymap in remap.lua
 -- Markdown Preview
