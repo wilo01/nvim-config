aa4d63183c3d56545d3a6ab1d9d340c58e5b90ac
Wilo01 <wilk9496@gmail.com>
2024-11-03 11:10:02 +0000

    JIRA: #Linux
    Logs:
    - Removed custom hints for which-key

--- nvim/.config/nvim/lua/DarO/lazy/which-key.lua
+++ nvim/.config/nvim/lua/DarO/lazy/which-key.lua
@@ -2,21 +2,6 @@ return {
    "folke/which-key.nvim",
    event = "VeryLazy",
    opts = {
-      defaults = {
-         ["z="] = { "Spell suggestions" },
-         ["zg"] = { "Add word to spellfile as good word" },
-         ["zG"] = { "Add word to internal word list as good word" },
-         ["zw"] = { "Mark word as wrong (bad) in spellfile" },
-         ["zW"] = { "Mark word as wrong (bad) in internal word list" },
-         ["zug"] = { "Remove word from spellfile as good/bad" },
-         ["zuG"] = { "Remove word from internal word list as good/bad" },
-         ["]s"] = { "Next misspelled word" },
-         ["[s"] = { "Previous misspelled word" },
-         ["]S"] = { "Next bad word only" },
-         ["[S"] = { "Previous bad word only" },
-         ["]r"] = { "Next rare word" },
-         ["[r"] = { "Previous rare word" },
-      },
    },
    keys = {
       {
