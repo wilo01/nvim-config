16e4d3854595461cc62865c2f6bd49ddeb1f72c9
Wilo01 <wilk9496@gmail.com>
2024-12-02 21:34:16 +0000

    JIRA: #Linux
    Logs:
    - Added snacks

--- nvim/.config/nvim/lua/DarO/lazy/snacks.lua
+++ nvim/.config/nvim/lua/DarO/lazy/snacks.lua
@@ -15,43 +15,47 @@ return {
       words = { enabled = true },
       styles = {
          notification = {
-            wo = { wrap = true } -- Wrap notifications
+            wo = { wrap = true }
          }
       }
    },
-   keys = {
-      { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Snacks Dismiss All Notifications" },
-      { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Snacks Delete Buffer" },
-      { "<leader>gg", function() Snacks.lazygit() end,                 desc = "Snacks Lazygit" },
-      { "<leader>gb", function() Snacks.git.blame_line() end,          desc = "Snacks Git Blame Line" },
-      { "<leader>gB", function() Snacks.gitbrowse() end,               desc = "Snacks Git Browse" },
-      { "<leader>gf", function() Snacks.lazygit.log_file() end,        desc = "Snacks Lazygit Current File History" },
-      { "<leader>gl", function() Snacks.lazygit.log() end,             desc = "Snacks Lazygit Log (cwd)" },
-      { "<leader>cR", function() Snacks.rename.rename_file() end,      desc = "Snacks Rename File" },
-      { "<leader>t",  function() Snacks.terminal() end,                desc = "Snacks Toggle Terminal" },
-      { "<leader>T",  function() Snacks.terminal() end,                desc = "Snacks which_key_ignore" },
-      { "<leader>]",  function() Snacks.words.jump(vim.v.count1) end,  desc = "Snacks Next Reference",              mode = { "n", "t" } },
-      { "<leader>[",  function() Snacks.words.jump(-vim.v.count1) end, desc = "Snacks Prev Reference",              mode = { "n", "t" } },
-      {
-         "<leader>N",
-         desc = "Neovim News",
-         function()
-            Snacks.win({
-               file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-               width = 0.6,
-               height = 0.6,
-               wo = {
-                  spell = false,
-                  wrap = false,
-                  signcolumn = "yes",
-                  statuscolumn = " ",
-                  conceallevel = 3,
-               },
-            })
-         end,
+   keys = function()
+      local Snacks = require("snacks")
+      return {
+         { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Snacks Dismiss All Notifications" },
+         { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Snacks Delete Buffer" },
+         { "<leader>gg", function() Snacks.lazygit() end,                 desc = "Snacks Lazygit" },
+         { "<leader>gb", function() Snacks.git.blame_line() end,          desc = "Snacks Git Blame Line" },
+         { "<leader>gr", function() Snacks.gitbrowse() end,               desc = "Snacks Git Browse" },
+         { "<leader>gf", function() Snacks.lazygit.log_file() end,        desc = "Snacks Lazygit Current File History" },
+         { "<leader>gl", function() Snacks.lazygit.log() end,             desc = "Snacks Lazygit Log (cwd)" },
+         { "<leader>cR", function() Snacks.rename.rename_file() end,      desc = "Snacks Rename File" },
+         { "<leader>t",  function() Snacks.terminal() end,                desc = "Snacks Toggle Terminal" },
+         { "<leader>T",  function() Snacks.terminal() end,                desc = "Snacks which_key_ignore" },
+         { "<leader>]",  function() Snacks.words.jump(vim.v.count1) end,  desc = "Snacks Next Reference",              mode = { "n", "t" } },
+         { "<leader>[",  function() Snacks.words.jump(-vim.v.count1) end, desc = "Snacks Prev Reference",              mode = { "n", "t" } },
+         {
+            "<leader>N",
+            desc = "Neovim News",
+            function()
+               Snacks.win({
+                  file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+                  width = 0.6,
+                  height = 0.6,
+                  wo = {
+                     spell = false,
+                     wrap = false,
+                     signcolumn = "yes",
+                     statuscolumn = " ",
+                     conceallevel = 3,
+                  },
+               })
+            end,
+         },
       }
-   },
+   end,
    init = function()
+      local Snacks = require("snacks") -- Require Snacks locally
       vim.api.nvim_create_autocmd("User", {
          pattern = "VeryLazy",
          callback = function()
